f5cf5fba72495eea4b056ad479fe5554ea39d042
Vishal Bharti <vishal.bharti@publicissapient.com>
2023-03-24 17:22:18 +0530

    bug fixes for java report

--- extension.js
+++ extension.js
@@ -11,112 +11,117 @@ const { Configuration, OpenAIApi } = require("openai");
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "testgpt" is now active!');
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "testgpt" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("testgpt.start", async () => {
-    // The code you place here will be executed every time your command is executed
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand("testgpt.start", async () => {
+		// The code you place here will be executed every time your command is executed
 
-    const gpt_use_case = await vscode.window.showQuickPick(
-      ["test", "code analysis", "code optimize suggestion"],
-      { placeHolder: "Select the view to show when opening a window." }
-    );
+		const gpt_use_case = await vscode.window.showQuickPick(
+			["test", "code analysis", "code optimize suggestion"],
+			{ placeHolder: "Select the view to show when opening a window." }
+		);
 
-    if (gpt_use_case == "test") {
-      const test_case_provider = await vscode.window.showQuickPick(
-        ["jest", "enzyme", "chai", "react-testing-library"],
-        { placeHolder: "Select the view to show when opening a window." }
-      );
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Generating Test Cases...");
+		if (gpt_use_case == "test") {
+			const test_case_provider = await vscode.window.showQuickPick(
+				["jest", "enzyme", "chai", "react-testing-library"],
+				{ placeHolder: "Select the view to show when opening a window." }
+			);
 
-      let currentFile = "";
-      if (vscode.workspace.workspaceFolders.length) {
-        currentFile = vscode.window.activeTextEditor.document.fileName;
-      }
+			// Display a message box to the user
+			vscode.window.showInformationMessage("Generating Test Cases...");
 
-      if (currentFile != "") {
-        fs.readFile(currentFile, async (err, data) => {
-          const content = data.toString();
-          let search = "";
-          let testFile = "";
-          if (currentFile.includes(".js")) {
-            search = `write only code with imports and no junk for unit test cases of javascript in a single file using ${test_case_provider} for \n`;
-            testFile = currentFile.replace(".js", ".test.js");
-          } else if (currentFile.includes(".ts")) {
-            search = `write only code with imports and no junk for unit test cases of typescript in a single file using ${test_case_provider} for \n`;
-            testFile = currentFile.replace(".ts", ".spec.ts");
-          }
-          const response = await createComplettion(search + content);
-          const code = response.data.choices[0].message.content;
-          fs.writeFile(testFile, code, (err) => {
-            if (err) console.log(err);
-            else {
-              vscode.window.showInformationMessage("Test Cases Generated");
-            }
-          });
-        });
-      }
-    } else {
-		vscode.window.showInformationMessage("Generating Report...");
+			let currentFile = "";
+			if (vscode.workspace.workspaceFolders.length) {
+				currentFile = vscode.window.activeTextEditor.document.fileName;
+			}
 
-		let currentFile = "";
-      if (vscode.workspace.workspaceFolders.length) {
-        currentFile = vscode.window.activeTextEditor.document.fileName;
-      }
+			if (currentFile != "") {
+				fs.readFile(currentFile, async (err, data) => {
+					const content = data.toString();
+					let search = "";
+					let testFile = "";
 
-      if (currentFile != "") {
-        fs.readFile(currentFile, async (err, data) => {
-          const content = data.toString();
-          let search = "";
-          let testFile = "";
-          if (currentFile.includes(".js")) {
-            search = `${gpt_use_case} for the following code and generate report \n`;
-            testFile = currentFile + ".report.txt";
-          } else if (currentFile.includes(".ts")) {
-            search = `${gpt_use_case} for the following code and generate report \n`;
-            testFile = currentFile + ".report.txt";
-          }
-          const response = await createComplettion(search + content);
-          const code = response.data.choices[0].message.content;
-          fs.writeFile(testFile, code, (err) => {
-            if (err) console.log(err);
-            else {
-              vscode.window.showInformationMessage("Report Generated");
-            }
-          });
-        });
-      }
-    }
-  });
+					if (currentFile.includes(".js")) {
+						search = `write only code with imports and no junk for unit test cases of javascript in a single file using ${test_case_provider} for \n`;
+						testFile = currentFile.replace(".js", ".test.js");
+					} else if (currentFile.includes(".ts")) {
+						search = `write only code with imports and no junk for unit test cases of typescript in a single file using ${test_case_provider} for \n`;
+						testFile = currentFile.replace(".ts", ".spec.ts");
+					}
 
-  context.subscriptions.push(disposable);
+					const response = await createComplettion(search + content);
+					const code = response.data.choices[0].message.content;
+
+					fs.writeFile(testFile, code, (err) => {
+						if (err) {
+							console.log(err);
+							vscode.window.showInformationMessage("Test Cases Generation Failed");
+						}
+						else {
+							vscode.window.showInformationMessage("Test Cases Generated");
+						}
+					});
+				});
+			}
+		} else {
+			vscode.window.showInformationMessage("Generating Report...");
+
+			let currentFile = "";
+			if (vscode.workspace.workspaceFolders.length) {
+				currentFile = vscode.window.activeTextEditor.document.fileName;
+			}
+
+			if (currentFile != "") {
+				fs.readFile(currentFile, async (err, data) => {
+					const content = data.toString();
+					let search = "";
+					let testFile = "";
+					search = `${gpt_use_case} for the following code and generate report \n`;
+					testFile = currentFile + ".report.txt";
+					const response = await createComplettion(search + content);
+					const code = response.data.choices[0].message.content;
+					fs.writeFile(testFile, code, (err) => {
+						if (err) {
+							console.log(err);
+							vscode.window.showInformationMessage("Report Generation Failed");
+						}
+						else {
+							vscode.window.showInformationMessage("Report Generated");
+						}
+					});
+				});
+			}
+		}
+	});
+
+	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
-  activate,
-  deactivate,
+	activate,
+	deactivate,
 };
 
 async function createComplettion(prompt) {
-  const configuration = new Configuration({
-    organization: "org-5vVbXjuRH940d1Dgpxg1brNw",
-    apiKey: "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",
-  });
+	const configuration = new Configuration({
+		organization: "org-5vVbXjuRH940d1Dgpxg1brNw",
+		apiKey: "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",
+	});
 
-  const openai = new OpenAIApi(configuration);
+	const openai = new OpenAIApi(configuration);
 
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: prompt }],
-  });
+	const response = await openai.createChatCompletion({
+		model: "gpt-3.5-turbo",
+		messages: [{ role: "user", content: prompt }],
+	});
 
-  return response;
+	return response;
 }

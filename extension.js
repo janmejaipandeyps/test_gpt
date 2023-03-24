619a643012a9a3dd2841300c3782640ed6d7a7fb
janmejaipandeyps <janmejai.pandey.ps@gmail.com>
2023-03-24 15:55:38 +0530

    fixed extension quick select

--- extension.js
+++ extension.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 const vscode = require("vscode");
 const { Configuration, OpenAIApi } = require("openai");
 
-
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
@@ -19,45 +18,43 @@ function activate(context) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "testgpt.helloWorld",
-    function () {
-      // The code you place here will be executed every time your command is executed
+  let disposable = vscode.commands.registerCommand("testgpt.test", async () => {
+    // The code you place here will be executed every time your command is executed
+    const test_case_provider = await vscode.window.showQuickPick(
+      ["jest", "enzyme", "chai", "react-testing-library"],
+      { placeHolder: "Select the view to show when opening a window." }
+    );
+    // Display a message box to the user
+    vscode.window.showInformationMessage("Generating Test Cases...");
 
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from TestGPT TESTT!");
-
-      let currentFile = "";
-      if (vscode.workspace.workspaceFolders.length) {
-        currentFile = vscode.window.activeTextEditor.document.fileName;
-      }
-
-      if (currentFile != "") {
-        fs.readFile(currentFile, async (err, data) => {
-			const content = data.toString();
-			let search = "";
-			let testFile = "";
-			if(currentFile.includes(".js")) {
-				search = "write only code for unit test cases in a single file using JEST and RTL for \n";
-				testFile = currentFile.replace(".js", ".test.js");
-			} else if(currentFile.includes(".ts")) {
-				search = "write only code for unit test cases in a single file using JEST for \n";
-				testFile = currentFile.replace(".ts", ".spec.ts");
-			}
-			const response = await createComplettion(search + content);
-			const code = response.data.choices[0].message.content;
-			fs.writeFile(testFile, code, (err) => {
-				if (err) console.log(err);
-				else {
-				console.log("File written successfully\n");
-				console.log("The written has the following contents:");
-				console.log(fs.readFileSync("books.txt", "utf8"));
-				}
-			});
-		});
-      }
+    let currentFile = "";
+    if (vscode.workspace.workspaceFolders.length) {
+      currentFile = vscode.window.activeTextEditor.document.fileName;
     }
-  );
+
+    if (currentFile != "") {
+      fs.readFile(currentFile, async (err, data) => {
+        const content = data.toString();
+        let search = "";
+        let testFile = "";
+        if (currentFile.includes(".js")) {
+          search = `write only code with imports and no junk for unit test cases of javascript in a single file using ${test_case_provider} for \n`;
+          testFile = currentFile.replace(".js", ".test.js");
+        } else if (currentFile.includes(".ts")) {
+          search = `write only code with imports and no junk for unit test cases of typescript in a single file using ${test_case_provider} for \n`;
+          testFile = currentFile.replace(".ts", ".spec.ts");
+        }
+        const response = await createComplettion(search + content);
+        const code = response.data.choices[0].message.content;
+        fs.writeFile(testFile, code, (err) => {
+          if (err) console.log(err);
+          else {
+            vscode.window.showInformationMessage("Test Cases Generated");
+          }
+        });
+      });
+    }
+  });
 
   context.subscriptions.push(disposable);
 }
@@ -71,18 +68,17 @@ module.exports = {
 };
 
 async function createComplettion(prompt) {
-	const configuration = new Configuration({
-		organization: "org-5vVbXjuRH940d1Dgpxg1brNw",
-		apiKey: "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",
-	  });
+  const configuration = new Configuration({
+    organization: "org-5vVbXjuRH940d1Dgpxg1brNw",
+    apiKey: "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",
+  });
 
-	  
-	const openai = new OpenAIApi(configuration);
+  const openai = new OpenAIApi(configuration);
 
-	const response = await openai.createChatCompletion({
-		model: "gpt-3.5-turbo",
-		messages: [{role: "user", content: prompt}]
-	});
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: prompt }],
+  });
 
-  	return response;
+  return response;
 }

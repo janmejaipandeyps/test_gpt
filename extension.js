3356283acb64e4a79ffc611eb864b2e3f7eaf228
janmejaipandeyps <janmejai.pandey.ps@gmail.com>
2023-03-24 16:57:39 +0530

    added code optimization

--- extension.js
+++ extension.js
@@ -18,41 +18,79 @@ function activate(context) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand("testgpt.test", async () => {
+  let disposable = vscode.commands.registerCommand("testgpt.start", async () => {
     // The code you place here will be executed every time your command is executed
-    const test_case_provider = await vscode.window.showQuickPick(
-      ["jest", "enzyme", "chai", "react-testing-library"],
+
+    const gpt_use_case = await vscode.window.showQuickPick(
+      ["test", "code analysis", "code optimize suggestion"],
       { placeHolder: "Select the view to show when opening a window." }
     );
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Generating Test Cases...");
 
-    let currentFile = "";
-    if (vscode.workspace.workspaceFolders.length) {
-      currentFile = vscode.window.activeTextEditor.document.fileName;
-    }
+    if (gpt_use_case == "test") {
+      const test_case_provider = await vscode.window.showQuickPick(
+        ["jest", "enzyme", "chai", "react-testing-library"],
+        { placeHolder: "Select the view to show when opening a window." }
+      );
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Generating Test Cases...");
 
-    if (currentFile != "") {
-      fs.readFile(currentFile, async (err, data) => {
-        const content = data.toString();
-        let search = "";
-        let testFile = "";
-        if (currentFile.includes(".js")) {
-          search = `write only code with imports and no junk for unit test cases of javascript in a single file using ${test_case_provider} for \n`;
-          testFile = currentFile.replace(".js", ".test.js");
-        } else if (currentFile.includes(".ts")) {
-          search = `write only code with imports and no junk for unit test cases of typescript in a single file using ${test_case_provider} for \n`;
-          testFile = currentFile.replace(".ts", ".spec.ts");
-        }
-        const response = await createComplettion(search + content);
-        const code = response.data.choices[0].message.content;
-        fs.writeFile(testFile, code, (err) => {
-          if (err) console.log(err);
-          else {
-            vscode.window.showInformationMessage("Test Cases Generated");
+      let currentFile = "";
+      if (vscode.workspace.workspaceFolders.length) {
+        currentFile = vscode.window.activeTextEditor.document.fileName;
+      }
+
+      if (currentFile != "") {
+        fs.readFile(currentFile, async (err, data) => {
+          const content = data.toString();
+          let search = "";
+          let testFile = "";
+          if (currentFile.includes(".js")) {
+            search = `write only code with imports and no junk for unit test cases of javascript in a single file using ${test_case_provider} for \n`;
+            testFile = currentFile.replace(".js", ".test.js");
+          } else if (currentFile.includes(".ts")) {
+            search = `write only code with imports and no junk for unit test cases of typescript in a single file using ${test_case_provider} for \n`;
+            testFile = currentFile.replace(".ts", ".spec.ts");
           }
+          const response = await createComplettion(search + content);
+          const code = response.data.choices[0].message.content;
+          fs.writeFile(testFile, code, (err) => {
+            if (err) console.log(err);
+            else {
+              vscode.window.showInformationMessage("Test Cases Generated");
+            }
+          });
         });
-      });
+      }
+    } else {
+		vscode.window.showInformationMessage("Generating Report...");
+
+		let currentFile = "";
+      if (vscode.workspace.workspaceFolders.length) {
+        currentFile = vscode.window.activeTextEditor.document.fileName;
+      }
+
+      if (currentFile != "") {
+        fs.readFile(currentFile, async (err, data) => {
+          const content = data.toString();
+          let search = "";
+          let testFile = "";
+          if (currentFile.includes(".js")) {
+            search = `${gpt_use_case} for the following code and generate report \n`;
+            testFile = currentFile + ".report.txt";
+          } else if (currentFile.includes(".ts")) {
+            search = `${gpt_use_case} for the following code and generate report \n`;
+            testFile = currentFile + ".report.txt";
+          }
+          const response = await createComplettion(search + content);
+          const code = response.data.choices[0].message.content;
+          fs.writeFile(testFile, code, (err) => {
+            if (err) console.log(err);
+            else {
+              vscode.window.showInformationMessage("Report Generated");
+            }
+          });
+        });
+      }
     }
   });
 
